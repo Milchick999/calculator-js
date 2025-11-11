e3d15b15e23eae5ebeccbf435b81a6d32a91e9f4
Zinaida Rudak <rudakzinaida@gmail.com>
2025-11-11 08:34:44 +0300

    Fix indentation in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,62 +2,62 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = (env, argv) => {
-    const isProduction = argv.mode === 'production';
+  const isProduction = argv.mode === 'production';
 
-    return {
-        entry: './src/index.js',
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: isProduction ? 'js/[name].[contenthash].js' : 'js/bundle.js',
-            clean: true,
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isProduction ? 'js/[name].[contenthash].js' : 'js/bundle.js',
+      clean: true,
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader',
+          ],
         },
-
-        module: {
-            rules: [
-                {
-                    test: /\.css$/,
-                    use: [
-                        'style-loader', 
-                        'css-loader',
-                    ],
-                },
-                {
-                    test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                    type: 'asset/resource',
-                },
-            ],
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
         },
+      ],
+    },
 
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: './src/index.html',
-                filename: 'index.html', 
-                title: 'Calculator', 
-                inject: 'body', 
-                minify: isProduction ? {
-                    collapseWhitespace: true,
-                    removeComments: true,
-                    removeRedundantAttributes: true,
-                    useShortDoctype: true,
-                } : false,
-            }),
-        ],
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+        title: 'Calculator',
+        inject: 'body',
+        minify: isProduction ? {
+          collapseWhitespace: true,
+          removeComments: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true,
+        } : false,
+      }),
+    ],
 
-        devServer: {
-            static: {
-                directory: path.join(__dirname, 'dist'), 
-            },
-            compress: true, 
-            port: 9000, 
-            open: true, 
-            hot: true, 
-        },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+      compress: true,
+      port: 9000,
+      open: true,
+      hot: true,
+    },
 
-        optimization: {
-             minimize: isProduction,
-        },
+    optimization: {
+      minimize: isProduction,
+    },
 
-        mode: isProduction ? 'production' : 'development',
-        devtool: isProduction ? false : 'source-map',
-    };
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? false : 'source-map',
+  };
 };
